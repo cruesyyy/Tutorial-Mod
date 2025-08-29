6bc2ddc84dff1323a44a8c832bf9e9cf847b25ad
Jan Wolter <janwolter2@gmail.com>
2025-08-29 22:59:56 +0200

    Added Pink Diamond

---
+++ src/main/java/de/cruesy/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package de.cruesy.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import de.cruesy.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PINK_DIAMOND = registerItem("pink_diamond", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_DIAMOND);
+        });
+    }
+}
